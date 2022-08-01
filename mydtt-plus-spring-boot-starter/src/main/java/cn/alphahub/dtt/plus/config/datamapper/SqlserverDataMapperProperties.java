a382bab7da25b3cd2203b2021f1b3d6c11fa8074
Weasley <1432689025@qq.com>
2022-08-01 20:34:02 +0800

    update

--- mydtt-plus-spring-boot-starter/src/main/java/cn/alphahub/dtt/plus/config/datamapper/SqlserverDataMapperProperties.java
+++ mydtt-plus-spring-boot-starter/src/main/java/cn/alphahub/dtt/plus/config/datamapper/SqlserverDataMapperProperties.java
@@ -15,7 +15,7 @@ import java.util.Properties;
 @ConfigurationProperties(prefix = "alphahub.dtt.data-type-mapper.sqlserver")
 public class SqlserverDataMapperProperties {
     /**
-     * Default character collation
+     * Default character collation, <a href="https://docs.microsoft.com/zh-cn/sql/relational-databases/collations/collation-and-unicode-support?view=sql-server-ver16">Here is the help link for SQLServer official.</a>
      */
     private String defaultCollate = "SQL_Latin1_General_CP1_CI_AS";
     /**
