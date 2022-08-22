b6845b9188852c284192c01937969a1bd9227f46
Weasley <1432689025@qq.com>
2022-08-22 21:32:02 +0800

    Added annotation for PrimaryKey and UniqueKey

---
+++ mydtt-plus-spring-boot-starter/src/main/java/cn/alphahub/dtt/plus/annotations/UniqueKey.java
@@ -0,0 +1,10 @@
+package cn.alphahub.dtt.plus.annotations;
+
+/**
+ * To annotate a field of a domain class is one of the unique key for table
+ *
+ * @author weasley
+ * @version 1.3.6
+ */
+public @interface UniqueKey {
+}
