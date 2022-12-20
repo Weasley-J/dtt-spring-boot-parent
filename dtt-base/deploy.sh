9cecd5a660239127262d74422ee9cdce2f8fbb1b
Weasley <1432689025@qq.com>
2022-12-20 13:47:43 +0800

    - update author email

--- dtt-base/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SETTINGS="/Users/weasley/Development/program/apache-maven/conf/settings-sonatype.xml"
-MODULE="dtt-base"
-
-clear &&
-  mvn clean deploy -pl :$MODULE -am --settings $SETTINGS &&
-  mvn clean
--- dtt-base/dtt-annotations/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SETTINGS="/Users/weasley/Development/program/apache-maven/conf/settings-sonatype.xml"
-MODULE="dtt-annotations"
-
-clear &&
-  mvn clean deploy -pl :$MODULE -am --settings $SETTINGS &&
-  mvn clean
