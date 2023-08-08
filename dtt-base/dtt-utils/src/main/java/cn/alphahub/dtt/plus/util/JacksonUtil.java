3e940c2c53bc43d536cbbfa39d41d9a3f121a513
Weasley <1432689025@qq.com>
2023-08-08 11:14:42 +0800

    [feature] Add Jackson original property naming strategy

--- dtt-base/dtt-utils/src/main/java/cn/alphahub/dtt/plus/util/JacksonUtil.java
+++ dtt-base/dtt-utils/src/main/java/cn/alphahub/dtt/plus/util/JacksonUtil.java
@@ -6,6 +6,12 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+import com.fasterxml.jackson.databind.introspect.AnnotatedClass;
+import com.fasterxml.jackson.databind.introspect.AnnotatedField;
+import com.fasterxml.jackson.databind.introspect.AnnotatedMethod;
+import com.fasterxml.jackson.databind.introspect.AnnotatedParameter;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
@@ -18,6 +24,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * Jackson工具类
@@ -126,4 +135,54 @@ public class JacksonUtil {
         }
     }
 
+    /**
+     * The Jackson Original property naming strategy
+     *
+     * @author weasley
+     * @since 1.1.0
+     */
+    public static class OriginalPropertyNamingStrategy extends PropertyNamingStrategies.NamingBase {
+
+        public static List<AnnotatedField> getAnnotatedFields(AnnotatedMethod method) {
+            @SuppressWarnings("deprecation") Iterable<AnnotatedField> fields = ((AnnotatedClass) method.getTypeContext()).fields();
+            return StreamSupport.stream(fields.spliterator(), false).collect(Collectors.toList());
+        }
+
+        @Override
+        public String translate(String propertyName) {
+            return propertyName;
+        }
+
+        @Override
+        public String nameForGetterMethod(MapperConfig<?> config, AnnotatedMethod method, String defaultName) {
+            List<AnnotatedField> annotatedFields = getAnnotatedFields(method);
+            for (AnnotatedField annotatedField : annotatedFields) {
+                if (defaultName.equalsIgnoreCase(annotatedField.getName())) {
+                    return annotatedField.getName();
+                }
+            }
+            return super.nameForGetterMethod(config, method, defaultName);
+        }
+
+        @Override
+        public String nameForField(MapperConfig<?> config, AnnotatedField field, String defaultName) {
+            return super.nameForField(config, field, defaultName);
+        }
+
+        @Override
+        public String nameForSetterMethod(MapperConfig<?> config, AnnotatedMethod method, String defaultName) {
+            List<AnnotatedField> annotatedFields = getAnnotatedFields(method);
+            for (AnnotatedField annotatedField : annotatedFields) {
+                if (defaultName.equalsIgnoreCase(annotatedField.getName())) {
+                    return annotatedField.getName();
+                }
+            }
+            return super.nameForSetterMethod(config, method, defaultName);
+        }
+
+        @Override
+        public String nameForConstructorParameter(MapperConfig<?> config, AnnotatedParameter ctorParam, String defaultName) {
+            return super.nameForConstructorParameter(config, ctorParam, defaultName);
+        }
+    }
 }
